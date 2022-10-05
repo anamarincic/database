d2043894a5c679a6376bda34c55dc913bbba11d1
anamarincic <amarincic5@gmail.com>
2022-10-05 18:20:12 +0200

    connect with real-time db

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+
 function App() {
+const [dogs, setDogs] = useState([]);
+
+useEffect(() => {
+  (() => {
+    return fetch("https://dog-related-application-default-rtdb.europe-west1.firebasedatabase.app/dogs.json")
+           .then((response) => { return response.json() });
+  })()
+  .then((data) => {setDogs(data.breed)})
+  .catch((error) => { console.log(error)});
+}, [dogs]);
+
+const dataDogs = dogs.map((dog) => (
+  <li id={dog.id}>
+    <h2>{dog.name}</h2>
+    <p>Origin: {dog.origin}</p>
+    </li>
+));
+    console.log(dataDogs);
+  
   return (
     <div className="App">
-      Hello
+      <h1>Dogs breed</h1>
+      <ul>
+        {dataDogs}
+      </ul>
     </div>
   );
 }
