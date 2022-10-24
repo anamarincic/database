1572aa47a0e37cbb90d58f236c00fcdbf7c5c9c6
anamarincic <amarincic5@gmail.com>
2022-10-24 15:09:27 +0200

    edit dog

--- src/components/ListOfDogs/ListOfDogs.jsx
+++ src/components/ListOfDogs/ListOfDogs.jsx
@@ -6,29 +6,6 @@ export function ListOfDogs(props) {
     return <h2>Loading...</h2>;
   }
 
-  /* const data = { name: "FILIP" };
-
-  const handleClick = (e) => {
-    const targetId = e.target.parentNode.id;
-    fetch(
-      `https://dog-related-application-default-rtdb.europe-west1.firebasedatabase.app/dogs/allDogs/${targetId}.json`,
-      {
-        method: `PATCH`,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  };*/
-
   const allDogs = props.posts.map((dog) => (
     <li key={dog.id} className="list__item" id={dog.id}>
       <h2 className="list__item__heading">{dog.name}</h2>
